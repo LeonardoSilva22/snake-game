d54f106178b983bbe868ca5cf1149a57091ed3f1
Leonardo Silva <leoscruz22@gmail.com>
2022-06-06 11:20:24 -0300

    first commit

---
+++ game.js
@@ -0,0 +1,87 @@
+//CRIANDO OS OBJETOS
+const snake = new Snake
+const apple = new Apple
+
+addEventListener('keydown',keyPressed)                      
+
+window.onload = () =>{
+    setInterval(gameLoop, 1000/15);
+}
+
+function gameLoop(){
+    draw()
+    update()
+}
+
+function draw(){
+    //CENÁRIO 
+    ctx.fillStyle = '#606c38'
+    ctx.fillRect(0,0,canvas.width,canvas.height)
+    //COBRA
+    snake.draw()
+    //MACÃ 
+    apple.draw()
+    //TEXTO DE PONTUACÃO
+    ctx.font =  `25px Gotham Rounded`
+    ctx.fillStyle = '#fefae0'
+    ctx.fillText(`Score: ${score}`,10,30)
+}
+function update(){
+    //MOVIMENTACÃO DA COBRA
+    snake.move()
+    //DERROTA CASO A COBRA COMA O PROPRIO RABO
+    snake.lose()
+    //COMER A MACÃ
+    eatApple()
+}
+
+function eatApple(){
+    for (let i = 0; i < snake.trail.length; i++) {
+        if(snake.trail[i].x == apple.x && snake.trail[i].y == apple.y){
+            snake.tail++
+            score++
+            apple.x = Math.floor(Math.random()*qt)
+            apple.y = Math.floor(Math.random()*qt)
+            //CONDICÃO PARA MACÃ NÃO SPAWNAR DENTRO DA COBRA
+            for (let i = 0; i < snake.trail.length; i++) {
+                if(apple.x == snake.trail[i].x*apple.size && apple.y == snake.trail[i].y*(apple.size+5))
+                    apple.x = Math.floor(Math.random()*qt)
+                    apple.y = Math.floor(Math.random()*qt)
+            }
+        }
+    } 
+}
+
+//EVENTO PARA MOVIMENTACÃO DA COBRA
+function keyPressed(e){
+    switch(String(e.key).toLocaleLowerCase()){
+        case 'a',"arrowleft":
+            if(snake.vx == snake.vel){
+                break
+            }
+            snake.vx = -snake.vel
+            snake.vy = 0
+            break
+        case 'w','arrowup':
+            if(snake.vy == snake.vel){
+                break
+            }
+            snake.vx = 0
+            snake.vy = -snake.vel
+            break
+        case 'd','arrowright':
+            if(snake.vx == -snake.vel){
+                break
+            }
+            snake.vx = snake.vel
+            snake.vy = 0   
+            break
+        case 's','arrowdown':
+            if(snake.vy == -snake.vel){
+                break
+            }
+            snake.vx = 0
+            snake.vy = snake.vel
+            break
+    }
+}
---
+++ scripts.js
@@ -0,0 +1,71 @@
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+const size = 30
+const qt = canvas.width / size
+let score = 0
+
+class Snake{
+    constructor(){
+        this.x = Math.floor(Math.random()*qt)
+        this.y = Math.floor(Math.random()*qt)
+        this.vel = 1
+        this.vx = this.vy = 0
+        this.trail = []
+        this.tail = 3
+    }
+    draw(){
+        for (let i = 0; i < this.trail.length; i++) {
+            createRect(this.trail[i].x * size,this.trail[i].y*size,size,size,'#283618')
+        }
+        this.trail.push({x:this.x,y:this.y})
+        //TAMANHO DA COBRA
+        while(this.trail.length > this.tail){
+            this.trail.shift()
+        }
+    }
+    move(){
+        this.x += this.vx
+        this.y += this.vy
+        
+        if(this.x > qt-1){
+            this.x = 0
+        }
+        if(this.y > qt-1){
+            this.y = 0
+        }
+        if(this.x < 0){
+            this.x = qt-1
+        }
+        if(this.y < 0){
+            this.y = qt-1
+        }
+    }
+    lose(){
+        for (let i = 0; i < this.trail.length; i++) {
+            if(this.trail[i].x == this.x && this.trail[i].y == this.y){
+                this.vx = this.vy = 0
+                score = 0
+                this.tail = 3      
+                break
+            }
+        }
+    }
+    
+}
+
+class Apple{
+    constructor(){
+        this.x = Math.floor(Math.random()*qt)
+        this.y = Math.floor(Math.random()*qt)
+        this.size = 20
+    }
+    draw(){
+        createRect(this.x*size,this.y*size,size,size,'#fb5607')
+    }
+}
+
+//FUNCÃO PARA CRIAR CONTEXTOS
+function createRect(x,y,width,height,color){
+    ctx.fillStyle = color
+    ctx.fillRect(x,y,width,height)
+}
